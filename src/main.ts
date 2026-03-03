6965fb6553bab72a05ba106338f2a660e2e91451
jsalazarv <jsalazarv8@gmail.com>
2026-03-03 12:57:15 -0600

    Revert "fix: cambiar puerto por defecto de 3000 a 3001"
    This reverts commit 180f871612e8c47d18202665a0838e9ddaa0980c.

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 3000;
   
   // Escuchar en 0.0.0.0 para permitir conexiones externas (Docker/Dokploy)
   await app.listen(port, '0.0.0.0');
